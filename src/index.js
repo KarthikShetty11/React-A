41d139a16613fc171eb5aaf9f58a41107b58af3e
Karthik Shetty <86142251+KarthikShetty11@users.noreply.github.com>
2022-04-04 13:57:36 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hi, I'm Karthik</h1>
+    <ul>
+      <li> part 1</li>
+      <li> part 2</li>
+      <li> part 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
