81437a8af26b7714cd9c07d2e65435e641ecc0a2
Karthik Shetty <86142251+KarthikShetty11@users.noreply.github.com>
2022-04-04 20:20:07 +0530

    JSX attributes, inline styling

--- src/index.js
+++ src/index.js
@@ -15,10 +15,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Karthik";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const customStyle = {
+  color: "grey",
+  fontSize: "1rem"
+};
+
 ReactDOM.render(
   <div>
-    <p>Created by Karthik</p>
-    <p>Copyright CurrentYear</p>
+    <h1 className="heading" contentEditable="false" spellCheck="false">
+      Hi, {name}
+    </h1>
+    <p style={{ color: "red" }}>Created by {name}</p>
+    <p style={customStyle}>Copyright {year} </p>
+    <div>
+      <img
+        className="img"
+        src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/cf4f8db9-3d2a-46de-a264-2bb944c682cc/revolution-6-next-nature-road-running-shoes-NC0P7k.png"
+        alt="shoeImage"
+      ></img>
+      <img
+        className="img"
+        src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/e8be7530-e1c8-4069-a469-3af53ba55009/downshifter-11-road-running-shoes-ggx1m6.png"
+        alt="shoeImage"
+      ></img>
+    </div>
   </div>,
   document.getElementById("root")
 );
