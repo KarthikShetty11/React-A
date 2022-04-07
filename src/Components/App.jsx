83467a360381cb528a5d8282c3d4ca18e7748870
Karthik Shetty <86142251+KarthikShetty11@users.noreply.github.com>
2022-04-07 16:28:44 +0530

    Calculator component Added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -13,10 +13,18 @@ import React from "react";
 import Lists from "./Lists";
 import Datee from "./Datee";
 import Content from "./Content";
+import { Add, Subtract, Multiply, Divide } from "./Calc";
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>{Add(5, 6)}</li>
+        <li>{Subtract(9, 5)}</li>
+        <li>{Multiply(2, 4)}</li>
+        <li>{Divide(6, 3)}</li>
+      </ul>
+
       <Datee />
       <Lists />
       <Content />
---
+++ src/Components/Calc.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Add(n1, n2) {
+  return n1 + n2;
+}
+
+function Subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function Multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function Divide(n1, n2) {
+  return n1 / n2;
+}
+
+export { Add, Subtract, Multiply, Divide };
